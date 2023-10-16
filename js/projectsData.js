9881476f0d13d6911a83852a18589eba993b0eca
surajkareppagol <surajkareppagol.dev@gmail.com>
2023-10-16 17:59:57 +0530

    📱 Update mobile version

--- js/projectsData.js
+++ js/projectsData.js
@@ -1,41 +1,14 @@
 const projectData = [
   {
-    title: "Rock Paper Scissor",
-    img: "https://raw.githubusercontent.com/surajkareppagol/assets-for-projects/main/rock_paper_scissor.gif",
-    alt: "Rock Paper Scissor Game Image",
-    about: "The classic rock paper scissor game built with VueJS.",
-    tools: ["JavaScript"],
+    title: "Libefile",
+    img: "https://raw.githubusercontent.com/surajkareppagol/assets-for-projects/046515a615ab8ab3f3569fe5a812b452ff5cc13d/libefile.gif",
+    alt: "Libefile Image",
+    about: "A Simple To Use File Library Built On Top Of C I/O Functions.",
+    tools: ["C", "Make", "Git"],
     links: [
       {
         link: "GitHub",
-        url: "https://github.com/surajkareppagol/rock-paper-scissors",
-      },
-    ],
-  },
-  {
-    title: "Pix Creator Web",
-    img: "https://raw.githubusercontent.com/surajkareppagol/assets-for-projects/main/pix_creator_web.gif",
-    alt: "Pix Creator Web Image",
-    about: "An Easy To Use Pixel Art Creator Built With Web Technologies.",
-    tools: ["JavaScript"],
-    links: [
-      {
-        link: "GitHub",
-        url: "https://github.com/surajkareppagol/pix-creator-web",
-      },
-    ],
-  },
-  {
-    title: "REM",
-    img: "https://raw.githubusercontent.com/surajkareppagol/assets-for-projects/main/rem.gif",
-    alt: "REM Image",
-    about:
-      "Render Markdown is an approach to provide a good and elegant look for the markdown files.",
-    tools: ["JavaScript"],
-    links: [
-      {
-        link: "GitHub",
-        url: "https://github.com/surajkareppagol/rem",
+        url: "https://github.com/surajkareppagol/libefile",
       },
     ],
   },
@@ -44,37 +17,15 @@ const projectData = [
     img: "https://raw.githubusercontent.com/surajkareppagol/assets-for-projects/main/tic_tac_toe.gif",
     alt: "Tic Tac Toe Image",
     about: "The classic tic tac toe game built With Web Technologies.",
-    tools: ["JavaScript"],
+    tools: ["JavaScript", "Sass"],
     links: [
       {
         link: "GitHub",
         url: "https://github.com/surajkareppagol/tic-tac-toe",
       },
-    ],
-  },
-  {
-    title: "Text Input",
-    img: "https://raw.githubusercontent.com/surajkareppagol/assets-for-projects/main/text_input.gif",
-    alt: "Text Input Image",
-    about: "A simple component written in JS, which takes text inputs.",
-    tools: ["JavaScript"],
-    links: [
       {
-        link: "GitHub",
-        url: "https://github.com/surajkareppagol/text-input",
-      },
-    ],
-  },
-  {
-    title: "Animation In C",
-    img: "https://raw.githubusercontent.com/surajkareppagol/assets-for-projects/main/animation_in_c.gif",
-    alt: "Animation In C Image",
-    about: "C functions for ASCII art animation.",
-    tools: ["C"],
-    links: [
-      {
-        link: "GitHub",
-        url: "https://github.com/surajkareppagol/animation-in-c",
+        link: "WebSite",
+        url: "https://surajkareppagol.github.io/tic-tac-toe",
       },
     ],
   },
@@ -91,6 +42,81 @@ const projectData = [
       },
     ],
   },
+  {
+    title: "Pix Creator Web",
+    img: "https://raw.githubusercontent.com/surajkareppagol/assets-for-projects/main/pix_creator_web.gif",
+    alt: "Pix Creator Web Image",
+    about: "An Easy To Use Pixel Art Creator Built With Web Technologies.",
+    tools: ["JavaScript", "Sass"],
+    links: [
+      {
+        link: "GitHub",
+        url: "https://github.com/surajkareppagol/pix-creator-web",
+      },
+      {
+        link: "WebSite",
+        url: "https://surajkareppagol.github.io/pix-creator-web/",
+      },
+    ],
+  },
+  {
+    title: "REM",
+    img: "https://raw.githubusercontent.com/surajkareppagol/assets-for-projects/main/rem.gif",
+    alt: "REM Image",
+    about:
+      "Render Markdown is an approach to provide a good and elegant look for the markdown files.",
+    tools: ["JavaScript", "NodeJS", "Express", "Sass"],
+    links: [
+      {
+        link: "GitHub",
+        url: "https://github.com/surajkareppagol/rem",
+      },
+    ],
+  },
+  {
+    title: "Numerical Methods",
+    img: "https://raw.githubusercontent.com/surajkareppagol/assets-for-projects/046515a615ab8ab3f3569fe5a812b452ff5cc13d/numerical_methods.gif",
+    alt: "Numerical Methods Image",
+    about: "Numerical Methods Programmed With C.",
+    tools: ["C"],
+    links: [
+      {
+        link: "GitHub",
+        url: "https://github.com/surajkareppagol/libefile",
+      },
+    ],
+  },
+  {
+    title: "Text Input",
+    img: "https://raw.githubusercontent.com/surajkareppagol/assets-for-projects/main/text_input.gif",
+    alt: "Text Input Image",
+    about: "A simple component written in JS, which takes text inputs.",
+    tools: ["JavaScript", "Sass"],
+    links: [
+      {
+        link: "GitHub",
+        url: "https://github.com/surajkareppagol/text-input",
+      },
+      {
+        link: "WebSite",
+        url: "https://surajkareppagol.github.io/text-input/",
+      },
+    ],
+  },
+  {
+    title: "Animation In C",
+    img: "https://raw.githubusercontent.com/surajkareppagol/assets-for-projects/main/animation_in_c.gif",
+    alt: "Animation In C Image",
+    about: "C functions for ASCII art animation.",
+    tools: ["C", "Make"],
+    links: [
+      {
+        link: "GitHub",
+        url: "https://github.com/surajkareppagol/animation-in-c",
+      },
+    ],
+  },
+
   {
     title: "C Programs",
     img: "https://raw.githubusercontent.com/surajkareppagol/assets-for-projects/7f04258483363a83e04948f343f43ccd0c560f6c/c_programs.gif",
@@ -109,7 +135,7 @@ const projectData = [
     img: "https://raw.githubusercontent.com/surajkareppagol/assets-for-projects/7f04258483363a83e04948f343f43ccd0c560f6c/opengl_biomes.gif",
     alt: "OpenGL Biomes Image",
     about: "Different Biomes Created In OpenGL.",
-    tools: ["C"],
+    tools: ["C", "OpenGL", "Make"],
     links: [
       {
         link: "GitHub",
@@ -132,37 +158,25 @@ const projectData = [
     ],
   },
   {
-    title: "Libefile",
-    img: "https://raw.githubusercontent.com/surajkareppagol/assets-for-projects/046515a615ab8ab3f3569fe5a812b452ff5cc13d/libefile.gif",
-    alt: "Libefile Image",
-    about: "A Simple To Use File Library Built On Top Of C I/O Functions.",
-    tools: ["C"],
+    title: "Rock Paper Scissor",
+    img: "https://raw.githubusercontent.com/surajkareppagol/assets-for-projects/main/rock_paper_scissor.gif",
+    alt: "Rock Paper Scissor Game Image",
+    about: "The classic rock paper scissor game built with VueJS.",
+    tools: ["JavaScript", "VueJS", "Sass"],
     links: [
       {
         link: "GitHub",
-        url: "https://github.com/surajkareppagol/libefile",
+        url: "https://github.com/surajkareppagol/rock-paper-scissors",
       },
     ],
   },
-  {
-    title: "Numerical Methods",
-    img: "https://raw.githubusercontent.com/surajkareppagol/assets-for-projects/046515a615ab8ab3f3569fe5a812b452ff5cc13d/numerical_methods.gif",
-    alt: "Numerical Methods Image",
-    about: "Numerical Methods Programmed With C.",
-    tools: ["C"],
-    links: [
-      {
-        link: "GitHub",
-        url: "https://github.com/surajkareppagol/libefile",
-      },
-    ],
-  },
+
   {
     title: "Sudoku C",
     img: "https://raw.githubusercontent.com/surajkareppagol/assets-for-projects/046515a615ab8ab3f3569fe5a812b452ff5cc13d/sudokuc.gif",
     alt: "Sudoku C Image",
     about: "The classic sudoku game built with C.",
-    tools: ["C"],
+    tools: ["C", "ncurses", "Make"],
     links: [
       {
         link: "GitHub",
@@ -175,7 +189,7 @@ const projectData = [
     img: "https://raw.githubusercontent.com/surajkareppagol/assets-for-projects/046515a615ab8ab3f3569fe5a812b452ff5cc13d/xml_json_parser.gif",
     alt: "XML To JSON Parser Image",
     about: "A python script which parses XML and converts it into JSON.",
-    tools: ["Python"],
+    tools: ["Python", "XML", "JSON"],
     links: [
       {
         link: "GitHub",
@@ -188,7 +202,7 @@ const projectData = [
     img: "https://raw.githubusercontent.com/surajkareppagol/assets-for-projects/main/pix_creator_app.gif",
     alt: "Pix Creator App Image",
     about: "An Easy To Use Pixel Art Creator Built For Android.",
-    tools: ["Java"],
+    tools: ["Java", "Android Studio"],
     links: [
       {
         link: "GitHub",
